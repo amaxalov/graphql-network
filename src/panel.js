d16ae116a3e528b176f2fb6f549272cab0f681d2
Alex Makhalov <aleksandr.maxalov@gmail.com>
2022-06-28 19:18:59 +0400

    TEST

--- src/panel.js
+++ src/panel.js
@@ -1,26 +1,37 @@
 /* global chrome */
-import DevToolsPanel from './components/DevToolsPanel';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import {
+  isGraphQL,
+  parseEntry,
+} from './lib/utils';
 
 let alreadyShown = false;
 
+const parseLogToState = (log) => {
+  if (!isGraphQL(log)) return null;
+  return parseEntry(log)
+    .then(data => {
+      window.testVariable = {...window.testVariable, data}
+      console.log(window.testVariable)
+    });
+};
+
+
+const requestHandler = (request) => {
+  parseLogToState(request);
+};
+
 function createPanel() {
   const theme = chrome.devtools.panels.themeName || 'default';
+
+  
+
   chrome.devtools.panels.create('GraphQL Network',
     './icons/icon48.png',
     './panel.html',
     (panel) => {
       panel.onShown.addListener((panelWindow) => {
         if (!alreadyShown) {
-          ReactDOM.render(
-            <DevToolsPanel
-              requestFinished={chrome.devtools.network.onRequestFinished}
-              getHAR={chrome.devtools.network.getHAR}
-              theme={theme}
-            />,
-            panelWindow.document.getElementById('results'),
-          );
+          chrome.devtools.network.onRequestFinished.addListener(requestHandler);
         }
         alreadyShown = true;
       });
